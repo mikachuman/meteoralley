b08784beb2c13be5c75b6f317f7c89fb261a4566
Mika Isomaa <mika.isomaa@gmail.com>
2015-08-12 14:57:52 +0300

    Initial commit

---
+++ Assets/adScript.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Advertisements;
+
+public class adScript : MonoBehaviour {
+    bool adsReady = false;
+    bool showingAd = false;
+
+    void Awake() {
+        if (Advertisement.isSupported) {
+            Advertisement.Initialize("61738", true);
+        } else {
+            Debug.Log("Platform not supported");
+        }
+        //showAd();
+    }
+
+    // Use this function to show the ad
+    public void showAd()
+    {
+        if (!showingAd)
+        {
+            showingAd = true;
+            StartCoroutine(pollAds());
+        }
+    }
+
+    IEnumerator pollAds()
+    {
+        while (!adsReady)
+        {
+            yield return new WaitForSeconds(1f);
+            if (Advertisement.IsReady())
+            {
+                Advertisement.Show();
+                adsReady = true;
+                showingAd = false;
+            }
+        }
+    }
+
+    
+
+    // ID: 61738
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/destroyAfterScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class destroyAfterScript : MonoBehaviour {
+    float seconds = 6f;
+	// Use this for initialization
+	void Start () {
+
+        Destroy(gameObject, seconds);
+	}
+}
